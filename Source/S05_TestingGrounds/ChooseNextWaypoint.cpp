bf09cfe30e05797f70371ac7612299521a07bf7b
LAPTOP-14F4RGDA\Apollo <stanley.nicholson@mail.com>
2017-10-22 11:34:47 -0400

    Behavior task for choose netx way point complete in cpp. TP cycling is waypoint

--- Source/S05_TestingGrounds/ChooseNextWaypoint.cpp
+++ Source/S05_TestingGrounds/ChooseNextWaypoint.cpp
@@ -2,12 +2,27 @@
 
 #include "S05_TestingGrounds.h"
 #include "ChooseNextWaypoint.h"
+#include "AIController.h"
+#include "PatrollingGuard.h"
 #include "BehaviorTree/BlackboardComponent.h"
 
 EBTNodeResult::Type UChooseNextWaypoint::ExecuteTask(UBehaviorTreeComponent &OwnerComp, uint8 * NodeMemory)
 {
+	//Get the patrol points
+	auto AIController = OwnerComp.GetAIOwner();
+	auto ControlledPawn = AIController->GetPawn();
+	auto PatrollinGuard = Cast<APatrollingGuard>(ControlledPawn);
+	auto PatrolPoints = PatrollinGuard->PatrolPointsCPP;
+
+	//Set next waypoint
 	auto BlackboardComp = OwnerComp.GetBlackboardComponent();
 	auto Index = BlackboardComp->GetValueAsInt(IndexKey.SelectedKeyName);
-	UE_LOG(LogTemp, Warning, TEXT("Yaypoint Index : %i"), Index)
+	BlackboardComp->SetValueAsObject(WaypointKey.SelectedKeyName, PatrolPoints[Index]);
+
+	//Cycle the index
+	auto NextIndex = (Index + 1) % PatrolPoints.Num();
+	BlackboardComp->SetValueAsInt(IndexKey.SelectedKeyName, NextIndex);
+
+	//UE_LOG(LogTemp, Warning, TEXT("Yaypoint Index : %i"), Index)
 	return EBTNodeResult::Succeeded;
 }
